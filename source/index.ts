c969c4fb7dea78b08ce94acd7f9d8293d8607dc3
Brian Faust <faustbrian@users.noreply.github.com>
2021-09-21 04:09:58 +0300

    fix: mark `verify` as `async`

--- source/index.ts
+++ source/index.ts
@@ -3,5 +3,5 @@ import argon2, { argon2id } from "argon2";
 export const hash = async (plain: string): Promise<string> =>
   argon2.hash(plain, { type: argon2id });
 
-export const verify = (hash: string, plain: string): Promise<boolean> =>
+export const verify = async (hash: string, plain: string): Promise<boolean> =>
   argon2.verify(hash, plain, { type: argon2id });
